fde772b160b3d6e7c4ab63d5339bdc4563c2ef6d
unknown <dadangdev@gmail.com>
2019-05-15 16:59:18 +0000

    first

---
+++ menu/footer.php
@@ -0,0 +1,11 @@
+<!-- footer -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    RPL Kelompok 1
+  </a>
+  <div class="container text-white"> 
+  Web Menu Kantin Itats By : RPL KELOMPOK 1 || Made With Bootstap 4</p> 
+  <a class="text-white" href="#">About Us</a>
+  <a class="text-white" href="#">Contact Us</a></div>
+</nav>
---
+++ menu/header.php
@@ -0,0 +1,77 @@
+<head>
+  
+
+    <title>Makan Apa Hari Ini?</title>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+  <a class="navbar-brand" href="#">MENU KANTIN ITATS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Makanan <span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="#">Minuman <span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="#">Rokok <span class="sr-only">(current)</span></a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="#">Camilan <span class="sr-only">(current)</span></a>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-success" type="submit">Search</button>
+    </form>
+  </div>
+  </div>
+</nav>
+  
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 80%;
+  }
+  </style>
+  
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/food2.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Ayam Geprek</h3>
+        <p>Pedasnya Bikin Nagis Banget!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/food3.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Nasi Padang</h3>
+        <p>Gurihnya Bikin Gak Bisa Berhenti</p>
+      </div>   
+    </div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>  
+
+  
+    
+  </head>
